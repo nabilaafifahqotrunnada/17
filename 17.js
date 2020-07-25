a0476f7c9c1af3661034c5a768895aaa3d063a47
nabilaafifahqotrunnada <nabila_afifah_28rpl@student.smktelkom-mlg.sch.id>
2020-07-26 01:16:30 +0700

    Third Commit

--- 17.js
+++ 17.js
@@ -6,12 +6,12 @@ let laptop =
     "Lenovo", "HP", "Acer", "Asus"
 ]
 
-//index
+//kondisi awal
 let i = 0
 
-//mengeksekusi selama kondisi true
+//mengeksekusi selama pengecekan kondisi true
 while(laptop[i]){
     console.log(laptop[i]);
     i++
 }
-//berhenti setelah eksekusi index ke 3 (eksekusi ke 4) karena kondisi false
+//berhenti setelah eksekusi ke 4 karena kondisi false

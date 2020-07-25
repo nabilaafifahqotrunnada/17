9f13899364425acb47fcbb81ae59db6ee0c9b6ac
nabilaafifahqotrunnada <nabila_afifah_28rpl@student.smktelkom-mlg.sch.id>
2020-07-25 23:53:30 +0700

    Second Commit

---
+++ 17.js
@@ -0,0 +1,17 @@
+//While Loop
+
+//membuat array laptop
+let laptop =
+[
+    "Lenovo", "HP", "Acer", "Asus"
+]
+
+//index
+let i = 0
+
+//mengeksekusi selama kondisi true
+while(laptop[i]){
+    console.log(laptop[i]);
+    i++
+}
+//berhenti setelah eksekusi index ke 3 (eksekusi ke 4) karena kondisi false
